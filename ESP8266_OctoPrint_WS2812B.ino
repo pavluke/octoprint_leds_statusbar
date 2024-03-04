e3b01680a905d08254f1798107da084ea63d403b
Pavluke <60747557+Pavluke@users.noreply.github.com>
2024-03-05 00:58:16 +0300

    -

--- ESP8266_OctoPrint_WS2812B.ino
+++ ESP8266_OctoPrint_WS2812B.ino
@@ -2,33 +2,24 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <FastLED.h>
+#include "defines.h"
 
 #define LED_PIN 2                  
 #define NUM_LEDS 23               
 CRGB leds[NUM_LEDS];
 
-const char* ssid = "Pavluke";          
-const char* password = "AL0ngPassw0rd";
+#define API_MTBS 10000
+
+const char* ssid = "RT-GPON-2580";          
+const char* password = "#10032023";
 WiFiClient client;
 
 IPAddress ip(192, 168, 0, 123);
-//char* octoprint_host = "octopi.example.com";  // Or your hostname. Comment out one or the other.
-const int octoprint_httpPort = 5000;  //If you are connecting through a router this will work, but you need a random port forwarded to the OctoPrint server from your router. Enter that port here if you are external
-String octoprint_apikey = "D3DAA0102D0748D8BEE59EBFD2432B2D"; //See top of file or GIT Readme about getting API key
+const int octoprint_httpPort = 5000;
+const String octoprint_apikey = "D3DAA0102D0748D8BEE59EBFD2432B2D";
+OctoprintApi api(client, ip, octoprint_httpPort, octoprint_apikey); 
 
-unsigned long api_mtbs = 10000;
-unsigned long api_lasttime = 0;
-byte connection_retry = 0;
-byte point = 0;
-byte octoErrors = 0;
-
-long printed_timeout = 3600000; //60 mins in milliseconds - timeout after printing completed, to clear strip
-long printed_timeout_timer = printed_timeout;
-
-// Use one of the following:
-//OctoprintApi api; //Be sure to call init in setup. 
-OctoprintApi api(client, ip, octoprint_httpPort, octoprint_apikey);               //If using IP address
-//OctoprintApi api(client, octoprint_host, octoprint_httpPort, octoprint_apikey); //If using hostname. Comment out one or the other.
+const unsigned long api_mtbs = 10000;
 
 void setup() {
   Serial.begin(115200);
@@ -47,23 +38,18 @@ void setup() {
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
+    static byte connection_retry = 0;
     delay(1000);
     Serial.print(".");
-    leds[(NUM_LEDS/2) - connection_retry] = CRGB(0,0,255);
-    leds[(NUM_LEDS/2) + connection_retry] = CRGB(0,0,255);
-    //delay(100);
+    leds[(NUM_LEDS / 2) - connection_retry] = CRGB(0,0,255);
+    leds[(NUM_LEDS / 2) + connection_retry] = CRGB(0,0,255);
     FastLED.show();
-    if (connection_retry == NUM_LEDS) {
-      ESP.reset();
-    }
-    else{
-      connection_retry++;
-    }
+    if (connection_retry == NUM_LEDS) ESP.reset();
+    else connection_retry++;
   }
  
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.println();
+  Serial.print("WiFi connected \n IP address: ");
   Serial.println(WiFi.localIP());
   fill_solid(leds, NUM_LEDS, 0);
   FastLED.show();
@@ -76,46 +62,34 @@ void loop() {
 String opState(){
   static String state;
   static String completion;
+  static unsigned long api_lasttime = 0;
+  static byte octoErrors;
   if (millis() - api_lasttime > api_mtbs || api_lasttime == 0) {
-    if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
-      if (api.getPrintJob()) { //Get the print job API endpoint
+    if (WiFi.status() == WL_CONNECTED) {
+      if (api.getPrintJob()) {
         state = api.printJob.printerState;
         completion = String(float(api.printJob.progressCompletion));
         octoErrors = 0;
-        if (api.printJob.printerState == "Printing") {
-          Serial.println("Printing: " + completion + "%");
+        if (api.printJob.printerState == PRINTING)    Serial.println("Printing: " + completion + "%");
+        else if (api.printJob.printerState == PAUSED) Serial.println("Paused: " + completion + "%");
+        else if (api.printJob.progressCompletion == 100 && api.printJob.printerState == OPERATIONAL) {
+          Serial.println(COMPLITED);
+          state =  COMPLITED;
         }
-        else if (api.printJob.printerState == "Paused"){
-          Serial.println("Paused: " + completion + "%");
+        else if (api.printJob.progressCompletion == 0 && api.printJob.printerState == OPERATIONAL) {
+          Serial.println(IDLE);
+          state =  IDLE;
         }
-        else if (api.printJob.progressCompletion == 100 && api.printJob.printerState == "Operational")
-        {
-          Serial.println("Complited");
-          state =  "Complited";
-        }
-        else if (api.printJob.progressCompletion == 0 && api.printJob.printerState == "Operational"){
-          Serial.println("Idle");
-          state =  "Idle";
-        }
-        else if (api.printJob.printerState == "Offline after error"){
-          Serial.println(state);
-        }
-        else if (api.printJob.printerState == "Offline"){
-          Serial.println(state);
-        }
+        else Serial.println(state);
       }
-      else{
-        octoErrors++;
-        if (octoErrors == 2){
-        }
-      }
-      api_lasttime = millis();  //Set api_lasttime to current milliseconds run
+      else octoErrors++;
+      api_lasttime = millis();
     }
   }
-  return state + ' ' + completion;
+  return state + " " + completion;
 }
 
-void LEDsController(String opState){
+void LEDsController(String opState) {
   const byte FULL_BRIGHTNESS = 255;
   const byte FULL_LEDSTRIP = 100;
   const byte RED = 0;
@@ -125,55 +99,53 @@ void LEDsController(String opState){
   String state = opState.substring(0,index);
   float completion = opState.substring(index).toFloat();
 
-  if (state == "Printing")                  solid(state, completion, GREEN, FULL_BRIGHTNESS, false, true);
-  else if (state == "Paused")               solid(state, completion, GREEN, FULL_BRIGHTNESS, true, true);
-  else if (state == "Complited")            solid(state, FULL_LEDSTRIP, GREEN, FULL_BRIGHTNESS, true, false);
-  else if (state == "Idle")                 solid(state, FULL_LEDSTRIP, GREEN, 20, false, false);
-  else if (state == "Offline after error")  solid(state, FULL_LEDSTRIP, RED, FULL_BRIGHTNESS, true, false);
-  else if (state == "Offline")              solid(state, FULL_LEDSTRIP, RED, 20, false, false);
+  if      (state == PRINTING)             solid(state, completion, GREEN, FULL_BRIGHTNESS, false, true);
+  else if (state == PAUSED)               solid(state, completion, GREEN, FULL_BRIGHTNESS, true, true);
+  else if (state == COMPLITED)            solid(state, FULL_LEDSTRIP, GREEN, FULL_BRIGHTNESS, true, false);
+  else if (state == IDLE)                 solid(state, FULL_LEDSTRIP, GREEN, 20, false, false);
+  else if (state == OFFLINE_AFTER_ERROR)  solid(state, FULL_LEDSTRIP, RED, FULL_BRIGHTNESS, true, false);
+  else if (state == OFFLINE)              solid(state, FULL_LEDSTRIP, RED, 20, false, false);
 }
 
-void solid(String state, float completion, byte color, byte brightness, bool blink, bool borders){
+void solid(String state, float completion, byte color, byte brightness, bool blink, bool borders) {
   // Яркость оставшейся части статус-бара
   byte SECONDARY_BRIGHTNESS = 20;
 
   static uint32_t timer;
   int DELAY = (brightness < 255) ? 1000 : 500;
-  int progress = NUM_LEDS * (completion-2) / 100;
+  int progress = NUM_LEDS * (completion - 2) / 100;
   int sub_progress = (int(completion) - completion) * 100;
-  if (sub_progress!=0){
-    if(sub_progress<50) sub_progress = -sub_progress;
-    else completion-=1;
-    sub_progress = sub_progress*255/100;
+  if (sub_progress != 0){
+    if(sub_progress < 50) sub_progress = -sub_progress;
+    else completion--;
+    sub_progress = sub_progress * 255 / 100;
   }
 
   if (!blink){
-    //fill_solid(leds, NUM_LEDS, CHSV(color, 255, SECONDARY_BRIGHTNESS));
     fill_solid(leds, progress, CHSV(color, 255, brightness));
     if(completion < 100) leds[progress] = CHSV(color, 255, sub_progress);
     if(borders){
-      leds[0] = CHSV(color+40, 255, 255);
-      leds[NUM_LEDS-1] = leds[0];
+      leds[0] = CHSV(color + 40, 255, 255);
+      leds[NUM_LEDS - 1] = leds[0];
     }
     FastLED.show();
   }
   else{
     if (millis() - timer < DELAY){
       fill_solid(leds, NUM_LEDS, 0);
-      leds[0] = CHSV(color+40, 255, 255);
-      leds[NUM_LEDS-1] = leds[0];
+      leds[0] = CHSV(color + 40, 255, 255);
+      leds[NUM_LEDS - 1] = leds[0];
       if(borders){
-        leds[0] = CHSV(color+40, 255, 255);
-        leds[NUM_LEDS-1] = leds[0];
+        leds[0] = CHSV(color + 40, 255, 255);
+        leds[NUM_LEDS - 1] = leds[0];
       }
     }
     else{
-      //fill_solid(leds+progress, NUM_LEDS, CHSV(color, 255, SECONDARY_BRIGHTNESS));
       fill_solid(leds, progress, CHSV(color, 255, 255));
       leds[progress] = CHSV(color, 255, sub_progress);
       if(borders){
-        leds[0] = CHSV(color+40, 255, 255);
-        leds[NUM_LEDS-1] = leds[0];
+        leds[0] = CHSV(color + 40, 255, 255);
+        leds[NUM_LEDS - 1] = leds[0];
       }
     }
     FastLED.show();
---
+++ defines.h
@@ -0,0 +1,8 @@
+// Status
+#define PRINTING "Printing"
+#define PAUSED "Paused"
+#define OPERATIONAL "Operational"
+#define COMPLITED "Complited"
+#define IDLE "Idle"
+#define OFFLINE_AFTER_ERROR "Offline after error"
+#define OFFLINE "Offline"
